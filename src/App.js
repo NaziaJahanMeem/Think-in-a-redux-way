27c9885c5253c518deacbcccb778a6f95f0793a5
NaziaJahanMeem <nazia.jahan@g.bracu.ac.bd>
2024-06-09 20:06:43 +0600

    1.2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter Application
+      </h1>
+
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
