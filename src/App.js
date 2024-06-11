23d5220439378cb2b38580726635e4518113727a
NaziaJahanMeem <nazia.jahan@g.bracu.ac.bd>
2024-06-11 13:32:12 +0600

    useDispatch()

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { Provider } from "react-redux";
 import Counter from "./components/Counter";
 import store from "./redux/store";
+import HooksCounter from "./components/HooksCounter";
 
 function App() {
   return (
     <Provider store={store}>
-      <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-        <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+      <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+        <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
           Simple Counter Application
         </h1>
 
-        <Counter />
+        <HooksCounter />
       </div>
     </Provider>
   );
--- src/components/HooksCounter.js
+++ src/components/HooksCounter.js
@@ -1,7 +1,18 @@
-import { connect } from "react-redux";
-import { increment } from "../redux/counter/actions";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../redux/counter/actions";
 
-function Counter({ count, increment, decrement }) {
+export default function HooksCounter() {
+  const count = useSelector((state) => state.value);
+  const dispatch = useDispatch();
+
+  const incrementHandler = (value) => {
+    dispatch(increment(value));
+  };
+
+  const decrementHandler = (value) => {
+    dispatch(decrement(value));
+  };
+
   return (
     <div className="max-w-md mx-auto mt-10 space-y-5">
       <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
@@ -12,14 +23,14 @@ function Counter({ count, increment, decrement }) {
           <button
             className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
             id="increment"
-            onClick={increment}
+            onClick={() => incrementHandler(5)}
           >
             Increment
           </button>
           <button
             className="bg-red-400 text-white px-3 py-2 rounded shadow"
             id="decrement"
-            onClick={decrement}
+            onClick={() => decrementHandler(2)}
           >
             Decrement
           </button>
@@ -28,18 +39,3 @@ function Counter({ count, increment, decrement }) {
     </div>
   );
 }
-const mapStateToProps = (state, ownProps) => {
-  // console.log(ownProps);
-  return {
-    count: state.value,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increment: (value) => dispatch(increment(value)),
-    decrement: (value) => dispatch(increment(value)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
