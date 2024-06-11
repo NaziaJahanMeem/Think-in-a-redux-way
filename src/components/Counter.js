c54e9052cc2eb78b46a1f0c94be1e992356bb9dc
NaziaJahanMeem <nazia.jahan@g.bracu.ac.bd>
2024-06-11 12:41:12 +0600

    useSelector

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,7 @@
-import { useState } from "react";
+import { connect } from "react-redux";
+import { increment } from "../redux/counter/actions";
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
-  const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
+function Counter({ count, increment, decrement }) {
   return (
     <div class="max-w-md mx-auto mt-10 space-y-5">
       <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
@@ -36,3 +28,17 @@ export default function Counter() {
     </div>
   );
 }
+const mapStateToProps = (state, ownProps) => {
+  return {
+    count: state.value,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: (value) => dispatch(increment(value)),
+    decrement: (value) => dispatch(increment(value)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
