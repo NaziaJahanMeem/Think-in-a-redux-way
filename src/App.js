c416e372b2080801a3918eed2dccd7c333c129ec
NaziaJahanMeem <nazia.jahan@g.bracu.ac.bd>
2024-06-11 11:09:57 +0600

    2.3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { Provider } from "react-redux";
 import Counter from "./components/Counter";
+import store from "./redux/store";
 
 function App() {
   return (
-    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-      <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
-        Simple Counter Application
-      </h1>
+    <Provider store={store}>
+      <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+        <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+          Simple Counter Application
+        </h1>
 
-      <Counter />
-    </div>
+        <Counter />
+      </div>
+    </Provider>
   );
 }
 
