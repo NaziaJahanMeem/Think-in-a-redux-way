c9dacd046a5649c03b8f9c5a6e94075bebc24699
NaziaJahanMeem <nazia.jahan@g.bracu.ac.bd>
2024-06-29 10:00:10 +0600

    context api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
-import AddVideo from "./components/AddVideo";
-import AllVideos from "./components/Allvideos";
-import Filters from "./components/Filters";
-import WatchedVideos from "./components/WatchedVideos";
+import { useState } from "react";
+import Cons from "./components/Cons";
+import Pros from "./components/Pros";
+import ProsCons from "./components/ProsCons";
+import DataContext from "./contexts/DataContext";
 
 function App() {
+    const [data, setData] = useState({ pros: 0, cons: 0 });
+    const { pros = 0, cons = 0 } = data || {};
+
     return (
-        <div className="w-screen h-screen p-20">
-            <div className="flex w-full">
-                <div className="w-full border border-slate-400 p-6 space-y-8">
-                    <div className="border-b py-2 border-slate-400 flex justify-between items-center">
-                        <span className="font-bold">All Videos</span>
-                        <Filters />
-                    </div>
-                    <AllVideos />
-                    <AddVideo />
-                </div>
-                <div className="w-full border border-slate-400 p-6 space-y-8">
-                    <div className="border-b py-2 border-slate-400">
-                        <span className="font-bold">Watched Videos</span>
-                    </div>
-                    <WatchedVideos />
+        <div className="w-screen h-screen p-10">
+            <div className="space-y-8">
+                <h1 className="font-bold text-xl">CONTEXT API</h1>
+                <h3 className="font-bold text-lg">Pros: {pros}</h3>
+                <h3 className="font-bold text-lg">Const: {cons}</h3>
+
+                <DataContext.Provider value={data}>
+                    <ProsCons />
+                    <Pros />
+                    <Cons />
+                </DataContext.Provider>
+
+                <div className="space-x-3">
+                    <button
+                        className="bg-green-600 px-3 py-2 text-white rounded"
+                        onClick={() =>
+                            setData((d) => ({ ...d, pros: d.pros + 1 }))
+                        }
+                    >
+                        Increment pros
+                    </button>
+                    <button
+                        className="bg-red-600 px-3 py-2 text-white rounded"
+                        onClick={() =>
+                            setData((d) => ({ ...d, cons: d.cons + 1 }))
+                        }
+                    >
+                        Increment cons
+                    </button>
                 </div>
             </div>
         </div>
